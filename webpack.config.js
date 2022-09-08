e9a48db3e85833c38cb8bb67ba538510e8cc23b1
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2022-09-08 09:35:21 +0400

    incorporated react into the project, installed webpack, transpiler, plugins and loaders necessary for a react enviroment.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new htmlWebpackPlugin({ template: "./public/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
