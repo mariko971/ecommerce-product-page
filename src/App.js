72134fb603e644689b72c3b6e92a13fc9316f12a
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2022-10-17 17:50:39 +0400

    Refactored the lightbox feature
    - replaced img tags for svg icons with svg elements to allow hover effect.
    - Gave app component max-width 0f 1440px

--- src/App.js
+++ src/App.js
@@ -100,19 +100,30 @@ function App() {
         {/* -------LIGHTBOX FEATURE------- */}
 
         <article className="modal">
-          <img
-            src="../images/icon-close.svg"
-            alt="close"
-            className="modal-close-btn"
-            onClick={() =>
-              (document.querySelector(".modal").style.transform = "scale(0)")
-            }
-          />
-          <Gallery
-            itemImages={data.itemImages}
-            itemThumbnails={data.itemThumbnails}
-            isModal={true}
-          />
+          <div className="modal-gallery-container">
+            {/* close button */}
+            <svg
+              className="modal-close-btn"
+              width="14"
+              height="15"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() =>
+                (document.querySelector(".modal").style.transform = "scale(0)")
+              }
+            >
+              <path
+                d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                fill="#fff"
+                fill-rule="evenodd"
+              />
+            </svg>
+
+            <Gallery
+              itemImages={data.itemImages}
+              itemThumbnails={data.itemThumbnails}
+              isModal={true}
+            />
+          </div>
         </article>
 
         {/* ----product details---- */}
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -16,22 +16,42 @@ const Gallery = ({ itemImages, itemThumbnails, isModal }) => {
       >
         {isModal ? (
           <>
-            <img
-              src="../images/icon-previous.svg"
-              alt="previous button"
+            {/* ---Modal button-previous--- */}
+            <svg
+              width="12"
+              height="18"
+              xmlns="http://www.w3.org/2000/svg"
               className="modal-btn prev-btn"
               onClick={() =>
                 setCurrent(current === 0 ? itemImages.length - 1 : current - 1)
               }
-            />
-            <img
-              src="../images/icon-next.svg"
-              alt="next button"
+            >
+              <path
+                d="M11 1 3 9l8 8"
+                stroke="#1D2026"
+                stroke-width="3"
+                fill="none"
+                fill-rule="evenodd"
+              />
+            </svg>
+            {/* ---modal button-next--- */}
+            <svg
+              width="13"
+              height="18"
+              xmlns="http://www.w3.org/2000/svg"
               className="modal-btn next-btn"
               onClick={() =>
                 setCurrent(current === itemImages.length - 1 ? 0 : current + 1)
               }
-            />
+            >
+              <path
+                d="m2 1 8 8-8 8"
+                stroke="#1D2026"
+                stroke-width="3"
+                fill="none"
+                fill-rule="evenodd"
+              />
+            </svg>
           </>
         ) : null}
       </div>
